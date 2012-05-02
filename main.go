86d9725d8ab3d481a10d04619c08b1abe28533a6
Naitik Shah <n@daaku.org>
2012-05-02 13:13:24 -0700

    working recompile and restart cycle

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
-	"strings"
-	"time"
+	"fmt"
+	"path/filepath"
+	"go/build"
+	"io/ioutil"
 	"github.com/howeyc/fsnotify"
 	"log"
 	"os"
@@ -14,31 +16,60 @@ import (
 var (
 	pattern = flag.String("f", ".",
 		"The regexp pattern to match file names against to watch for changes.")
-	commandStr = flag.String("c", "", "The command to run.")
 	verbose = flag.Bool("v", false, "Verbose mode.")
 
-	command string
+	pkg *build.Package
+	goBin string
+	commandBin string
+	process *os.Process
 	commandArgv []string
 )
 
 func init() {
 	flag.Parse()
-	commandArgv = strings.Split(*commandStr, " ")
-	if len(commandArgv) == 0 {
-		log.Fatalf("A command must be specified.")
-	}
-	var err error
-	command, err = exec.LookPath(commandArgv[0])
+	cwd, err := os.Getwd()
 	if err != nil {
-		log.Fatalf("Error finding binary for %s", *commandStr)
+		cwd = "/"
 	}
+	log.Println(flag.Args())
+	pkg, err = build.Import(flag.Arg(0), cwd, build.FindOnly)
+	if err != nil {
+		log.Fatalf("Can't load package: %s", err)
+	}
+	goBin, err = exec.LookPath("go")
+	if err != nil {
+		log.Fatalf("Error finding go binary: %s", err)
+	}
+	log.Printf("%+v\n", pkg)
+}
+
+func basename() string {
+	return filepath.Base(pkg.ImportPath)
+}
+
+func compile() error {
+	tempFile, err := ioutil.TempFile("", basename())
+	if err != nil {
+		return err
+	}
+	_ = os.Remove(tempFile.Name()) // the build tool will create this
+	cmd := exec.Command(goBin, "build", "-o", tempFile.Name(), pkg.ImportPath)
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		return fmt.Errorf(
+			"Failed to build package %s: %s", pkg.ImportPath, string(out))
+	}
+	commandBin = tempFile.Name()
+	return nil
 }
 
 func run() (process *os.Process, err error) {
 	if *verbose {
-		log.Printf("Running command %s with args %v", command, commandArgv)
+		log.Printf("Running command %s with args %v", commandBin, commandArgv)
 	}
-	return os.StartProcess(command, commandArgv, &os.ProcAttr{
+	argsWithName := []string{basename()}
+	argsWithName = append(argsWithName, commandArgv...)
+	return os.StartProcess(commandBin, argsWithName, &os.ProcAttr{
 		Files: []*os.File{
 			nil,
 			os.Stdout,
@@ -55,10 +86,6 @@ func main() {
 	if err != nil {
 		log.Fatal("Failed to compile given regexp pattern: %s", *pattern)
 	}
-	pwd, err := os.Getwd()
-	if err != nil {
-		log.Fatal(err)
-	}
 	if *verbose {
 		log.Printf("Creating watcher.")
 	}
@@ -67,9 +94,16 @@ func main() {
 		log.Fatal(err)
 	}
 	if *verbose {
-		log.Printf("Watching %s.", pwd)
+		log.Printf("Watching %s.", pkg.Dir)
 	}
-	err = watcher.Watch(pwd)
+	err = watcher.Watch(pkg.Dir)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if *verbose {
+		log.Printf("About to compile.")
+	}
+	err = compile()
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -93,9 +127,13 @@ func main() {
 				if *verbose {
 					log.Println("Change triggered restart.")
 				}
+				err = compile()
+				if err != nil {
+					log.Println("Ignoring failed compile: %s", err)
+					continue
+				}
 				process.Kill()
 				process.Wait()
-				time.Sleep(500 * time.Millisecond)
 				process, err = run()
 				if err != nil {
 					log.Fatal(err)
