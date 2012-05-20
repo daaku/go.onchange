274708a3ccf8ecafed141726af6ed9f622c15054
Naitik Shah <n@daaku.org>
2012-05-20 15:22:27 -0700

    split code into pkgwatcher and tool libraries

--- main.go
+++ main.go
@@ -1,17 +1,17 @@
 // Command go.onchange automates the compile-restart cycle for developing
 // go based servers by monitoring for changes in source files and
-// dependencies.
+// dependencies. It can additionally also install packages as they
+// change in order to allow the gocode autocomplete daemon to be able
+// to find upto date information.
 package main
 
 import (
 	"flag"
-	"fmt"
-	"github.com/howeyc/fsnotify"
-	"go/build"
+	"github.com/nshah/go.pkgwatcher"
+	"github.com/nshah/go.tool"
 	"io/ioutil"
 	"log"
 	"os"
-	"os/exec"
 	"path/filepath"
 	"regexp"
 )
@@ -19,158 +19,85 @@ import (
 var (
 	pattern = flag.String("f", ".",
 		"The regexp pattern to match file names against to watch for changes.")
+	installDeps = flag.Bool("i", true, "Install changed library packages.")
 	verbose = flag.Bool("v", false, "Verbose mode.")
 
-	pkg         *build.Package
-	goBin       string
-	process     *os.Process
-	commandBin  string
-	commandArgv []string
+	process *os.Process
 )
 
-func init() {
-	flag.Parse()
-	cwd, err := os.Getwd()
+// Compile & Run.
+func restart(importPath string, args []string) {
+	basename := filepath.Base(importPath)
+	tempFile, err := ioutil.TempFile("", basename+"-")
 	if err != nil {
-		cwd = "/"
+		log.Print("Error creating temp file: %s", err)
+		return
 	}
-	pkg, err = build.Import(flag.Arg(0), cwd, build.AllowBinary)
+	tempFileName := tempFile.Name()
+	_ = os.Remove(tempFileName) // the build tool will create this
+	options := tool.Options{
+		ImportPaths: []string{importPath},
+		Output: tempFileName,
+	}
+	affected, err := options.Command("build")
+	defer os.Remove(tempFileName)
 	if err != nil {
-		log.Fatalf("Can't load package: %s", err)
+		log.Print(err)
+		return
 	}
-	goBin, err = exec.LookPath("go")
-	if err != nil {
-		log.Fatalf("Error finding go binary: %s", err)
-	}
-	commandArgv = flag.Args()[1:flag.NArg()]
-}
-
-func basename() string {
-	return filepath.Base(pkg.ImportPath)
-}
-
-func compile() error {
-	tempFile, err := ioutil.TempFile("", basename()+"-")
-	if err != nil {
-		return err
-	}
-	_ = os.Remove(tempFile.Name()) // the build tool will create this
-	cmd := exec.Command(goBin, "build", "-o", tempFile.Name(), pkg.ImportPath)
-	out, err := cmd.CombinedOutput()
-	if err != nil {
-		return fmt.Errorf(
-			"Failed to build package %s: %s", pkg.ImportPath, string(out))
-	}
-	commandBin = tempFile.Name()
-	return nil
-}
-
-func contains(list []string, item string) bool {
-	for _, i := range list {
-		if item == i {
-			return true
+	if len(affected) == 0 {
+		if *verbose {
+			log.Print("Ignoring rebuild with zero affected packages.")
 		}
+		return // nothing was changed, don't restart
 	}
-	return false
-}
-
-func dirsToWatch(pkg *build.Package) []string {
-	return dirsToWatchHelper([]string{}, pkg)
-}
-
-func dirsToWatchHelper(dirs []string, pkg *build.Package) []string {
-	if contains(dirs, pkg.Dir) {
-		return dirs
+	if process != nil {
+		process.Kill()
+		process.Wait()
+		process = nil
 	}
-	for _, path := range pkg.Imports {
-		imp, err := build.Import(path, pkg.Dir, build.AllowBinary)
-		if err == nil {
-			dirs = dirsToWatchHelper(dirs, imp)
-		} else if *verbose {
-			log.Printf(
-				"Skipping unresolvable import path %s with error %s", path, err)
-		}
-	}
-	filepath.Walk(pkg.Dir, func(path string, info os.FileInfo, err error) error {
-		if filepath.Base(info.Name())[0] == '.' {
-			return filepath.SkipDir
-		}
-		if info.IsDir() && !contains(dirs, path) {
-			dirs = append(dirs, path)
-		}
-		return nil
-	})
-	return dirs
-}
-
-func run() (process *os.Process, err error) {
-	if *verbose {
-		log.Printf("Running command %s with args %v", commandBin, commandArgv)
-	}
-	argsWithName := []string{basename()}
-	argsWithName = append(argsWithName, commandArgv...)
-	p, e := os.StartProcess(commandBin, argsWithName, &os.ProcAttr{
+	process, err = os.StartProcess(tempFileName, args, &os.ProcAttr{
 		Files: []*os.File{
 			nil,
 			os.Stdout,
 			os.Stderr,
 		},
 	})
-	_ = os.Remove(commandBin)
-	return p, e
+	if err != nil {
+		log.Printf("Failed to run command: %s", err)
+	}
+}
+
+// Install a library package.
+func install(importPath string) {
+	options := tool.Options{
+		ImportPaths: []string{importPath},
+	}
+	_, err := options.Command("install")
+	// only output in verbose since restart() will most likely echo this too.
+	if *verbose && err != nil {
+		log.Print(err)
+	}
 }
 
 func main() {
-	if *verbose {
-		log.Printf("Welcome.")
-	}
+	flag.Parse()
+	importPath := flag.Arg(0)
+	args := append(
+		[]string{filepath.Base(importPath)}, flag.Args()[1:flag.NArg()]...)
+
 	re, err := regexp.Compile(*pattern)
 	if err != nil {
 		log.Fatal("Failed to compile given regexp pattern: %s", *pattern)
 	}
-	if *verbose {
-		log.Printf("Creating watcher.")
-	}
-	watcher, err := fsnotify.NewWatcher()
+	watcher, err := pkgwatcher.NewWatcher([]string{importPath}, "")
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	go func() {
-		dirs := dirsToWatch(pkg)
-		for _, d := range dirs {
-			if *verbose {
-				log.Printf("Watching %s", d)
-			}
-			err = watcher.Watch(d)
-			if err != nil {
-				log.Fatal(err)
-			}
-		}
-	}()
-	if *verbose {
-		log.Printf("About to compile.")
-	}
-	err = compile()
-	if err != nil {
-		log.Fatal(err)
-	}
-	if *verbose {
-		log.Printf("About to run command.")
-	}
-	process, err := run()
-	if err != nil {
-		log.Fatal(err)
-	}
-	if *verbose {
-		log.Printf("Waiting for changes with pattern: %s\n", *pattern)
-	}
+	restart(importPath, args)
 	for {
 		select {
 		case ev := <-watcher.Event:
-			if *verbose {
-				log.Println("event:", ev)
-			}
 			if filepath.Base(ev.Name)[0] == '.' {
 				if *verbose {
 					log.Printf("Ignored changed dot file %s", ev.Name)
@@ -179,16 +106,12 @@ func main() {
 				if *verbose {
 					log.Printf("Change triggered restart: %s", ev.Name)
 				}
-				err = compile()
-				if err != nil {
-					log.Printf("Ignoring failed compile: %s", err)
-					continue
-				}
-				process.Kill()
-				process.Wait()
-				process, err = run()
-				if err != nil {
-					log.Fatal(err)
+				restart(importPath, args)
+				if *installDeps && !ev.Package.IsCommand() {
+					if *verbose {
+						log.Printf("Installing changed package: %s", ev.Package.ImportPath)
+					}
+					install(ev.Package.ImportPath)
 				}
 			} else {
 				if *verbose {
