6c56a3008c7a17ccacc4fee434f94bd01f81d37f
Naitik Shah <n@daaku.org>
2012-08-31 05:56:41 -0700

    initial work to eliminate global state

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 // Command go.onchange automates the compile-restart-test cycle for
 // developing go based applications by monitoring for changes in source
 // files and dependencies.
-
+//
 // It will:
 //     - Install packages.
 //     - Restart application.
@@ -22,18 +22,20 @@ import (
 	"sync"
 )
 
-var (
-	pattern = flag.String("f", ".",
-		"regexp pattern to match file names against to watch for changes")
-	installAll  = flag.Bool("i", true, "install all packages on change")
-	runTests    = flag.Bool("t", true, "run tests on change")
-	verbose     = flag.Bool("v", false, "verbose")
-	clearEnable = flag.Bool("c", true, "clear on restart")
-	eventLock   = new(sync.Mutex)
-
+// The main Monitor instance.
+type Monitor struct {
+	WatchPattern     string
+	RunTests         bool
+	Verbose          bool
+	ClearScreen      bool
+	IncludePattern   *regexp.Regexp
+	Watcher          *pkgwatcher.Watcher
+	ImportPath       string
+	Args             []string
+	eventLock        sync.Locker
 	lastCommandError *tool.CommandError
 	process          *os.Process
-)
+}
 
 type restartResult int
 
@@ -43,34 +45,35 @@ const (
 	restartBuildFailed
 )
 
-func clear() {
-	if *clearEnable {
+func (m *Monitor) clear() {
+	if m.ClearScreen {
 		fmt.Printf("\033[2J")
 		fmt.Printf("\033[H")
 	}
 }
 
 // Checks and also updates the last command.
-func isSameAsLastCommandError(err error) bool {
+func (m *Monitor) isSameAsLastCommandError(err error) bool {
 	commandError, ok := err.(*tool.CommandError)
 	if !ok {
 		return false
 	}
-	if lastCommandError != nil && string(lastCommandError.StdErr()) == string(commandError.StdErr()) {
+	last := m.lastCommandError
+	if last != nil && string(last.StdErr()) == string(commandError.StdErr()) {
 		return true
 	}
-	lastCommandError = commandError
+	m.lastCommandError = commandError
 	return false
 }
 
 // Compile & Run.
-func restart(importPath string, args []string) (result restartResult) {
-	if *verbose {
+func (m *Monitor) restart(importPath string, args []string) (result restartResult) {
+	if m.Verbose {
 		log.Print("restart requested")
 	}
 	result = restartBuildFailed
 	defer func() {
-		if *verbose {
+		if m.Verbose {
 			log.Printf("restart result: %d", result)
 		}
 	}()
@@ -88,37 +91,37 @@ func restart(importPath string, args []string) (result restartResult) {
 		Verbose:     true,
 	}
 	affected, err := options.Command("build")
-	if *verbose {
+	if m.Verbose {
 		log.Printf("Affected: %v", affected)
 	}
 	defer os.Remove(tempFileName)
 	if err != nil {
-		if isSameAsLastCommandError(err) {
-			if *verbose {
+		if m.isSameAsLastCommandError(err) {
+			if m.Verbose {
 				log.Printf("ignoring same as last command error: %s", err)
 			}
 			result = restartUnnecessary
 			return
 		}
-		clear()
+		m.clear()
 		log.Print(err)
 		result = restartBuildFailed
 		return
 	}
-	if process != nil && len(affected) == 0 {
-		if *verbose {
+	if m.process != nil && len(affected) == 0 {
+		if m.Verbose {
 			log.Print("Ignoring rebuild with zero affected packages.")
 		}
 		result = restartUnnecessary // nothing was changed, don't restart
 		return
 	}
-	clear()
-	if process != nil {
-		process.Kill()
-		process.Wait()
-		process = nil
+	m.clear()
+	if m.process != nil {
+		m.process.Kill()
+		m.process.Wait()
+		m.process = nil
 	}
-	process, err = os.StartProcess(tempFileName, args, &os.ProcAttr{
+	m.process, err = os.StartProcess(tempFileName, args, &os.ProcAttr{
 		Files: []*os.File{
 			nil,
 			os.Stdout,
@@ -135,13 +138,13 @@ func restart(importPath string, args []string) (result restartResult) {
 }
 
 // Install a library package.
-func install(importPath string) restartResult {
+func (m *Monitor) install(importPath string) restartResult {
 	options := tool.Options{
 		ImportPaths: []string{importPath},
 		Verbose:     true,
 	}
 	affected, err := options.Command("install")
-	if *verbose {
+	if m.Verbose {
 		if err != nil {
 			log.Printf("Install Error: %v", err)
 		} else {
@@ -155,91 +158,88 @@ func install(importPath string) restartResult {
 }
 
 // Test a package.
-func test(importPath string) {
+func (m *Monitor) test(importPath string) {
 	options := tool.Options{
 		ImportPaths: []string{importPath},
 	}
 	_, err := options.Command("test")
-	if err != nil && !isSameAsLastCommandError(err) {
+	if err != nil && !m.isSameAsLastCommandError(err) {
 		log.Print(err)
 	}
 }
 
-type Monitor struct {
-	IncludePattern *regexp.Regexp
-	Watcher        *pkgwatcher.Watcher
-	ImportPath     string
-	Args           []string
-}
-
 // Watcher event handler.
 func (m *Monitor) event(ev *pkgwatcher.Event) {
-	eventLock.Lock()
-	defer eventLock.Unlock()
+	m.eventLock.Lock()
+	defer m.eventLock.Unlock()
 	if filepath.Base(ev.Name)[0] == '.' {
-		if *verbose {
+		if m.Verbose {
 			log.Printf("Ignored changed dot file %s", ev.Name)
 		}
 	} else if m.IncludePattern.Match([]byte(ev.Name)) {
-		if *verbose {
+		if m.Verbose {
 			log.Printf("Change triggered restart: %s", ev.Name)
 		}
 		var installR restartResult
-		if *installAll {
-			if *verbose {
-				log.Printf("Installing all packages.")
-			}
-			installR = install("all")
-		} else {
-			installR = install(m.ImportPath)
+		if m.Verbose {
+			log.Printf("Installing all packages.")
 		}
+		installR = m.install("all")
 		if installR == restartUnnecessary {
-			if *verbose {
+			if m.Verbose {
 				log.Printf("Skipping because did not install anything.")
 			}
 			return
 		}
-		restartR := restart(m.ImportPath, m.Args)
+		restartR := m.restart(m.ImportPath, m.Args)
 		if restartR == restartUnnecessary {
 			return
 		}
 		go m.Watcher.WatchImportPath(ev.Package.ImportPath, true)
-		if *runTests {
-			if *verbose {
+		if m.RunTests {
+			if m.Verbose {
 				log.Printf("Testing %s.", ev.Package.ImportPath)
 			}
-			test(ev.Package.ImportPath)
+			m.test(ev.Package.ImportPath)
 		}
 	} else {
-		if *verbose {
+		if m.Verbose {
 			log.Printf("Ignored changed file %s", ev.Name)
 		}
 	}
 }
 
 func main() {
+	monitor := &Monitor{
+		eventLock: new(sync.Mutex),
+	}
+	flag.StringVar(&monitor.WatchPattern, "f", ".",
+		"regexp pattern to match file names against to watch for changes")
+	flag.BoolVar(&monitor.RunTests, "t", true, "run tests on change")
+	flag.BoolVar(&monitor.Verbose, "v", false, "verbose")
+	flag.BoolVar(&monitor.ClearScreen, "c", true, "clear screen on restart")
 	flag.Parse()
+
 	importPath := flag.Arg(0)
 	args := append(
 		[]string{filepath.Base(importPath)}, flag.Args()[1:flag.NArg()]...)
 
-	re, err := regexp.Compile(*pattern)
+	re, err := regexp.Compile(monitor.WatchPattern)
 	if err != nil {
-		log.Fatal("Failed to compile given regexp pattern: %s", *pattern)
+		log.Fatal("Failed to compile given regexp pattern: %s", monitor.Watcher)
 	}
 	watcher, err := pkgwatcher.NewWatcher([]string{importPath}, "")
 	if err != nil {
 		log.Fatal(err)
 	}
-	monitor := &Monitor{
-		ImportPath:     importPath,
-		IncludePattern: re,
-		Watcher:        watcher,
-		Args:           args,
-	}
-	restart(importPath, args)
+
+	monitor.ImportPath = importPath
+	monitor.IncludePattern = re
+	monitor.Watcher = watcher
+	monitor.Args = args
+	monitor.restart(importPath, args)
 	for {
-		if *verbose {
+		if monitor.Verbose {
 			log.Printf("Main loop iteration.")
 		}
 		select {
